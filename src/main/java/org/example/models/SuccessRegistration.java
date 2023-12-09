c4652ad7683a978ec43fe3ba4a683b7a04f6c65a
Basil Akavity <vakavity@gmail.com>
2023-12-09 19:09:20 +0300

    added pojo classes

---
+++ src/main/java/org/example/models/SuccessRegistration.java
@@ -0,0 +1,13 @@
+package org.example.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class SuccessRegistration {
+    private Integer id;
+    private String token;
+}
---
+++ src/main/java/org/example/models/UnSuccessReg.java
@@ -0,0 +1,12 @@
+package org.example.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UnSuccessReg {
+    private String error;
+}
